ef5dab29fac16abe3c0b555c693ee9b175fc0948
janetuyy <118744357+janetuyy@users.noreply.github.com>
2023-07-16 16:04:08 +0300

    second meow

--- turkisHome/main/urls.py
+++ turkisHome/main/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='main'),
+    path('index', views.index, name='main'),
+    path('flats', views.flats, name='flats'),
+    path('new', views.new, name='new'),
+
 ]
--- turkisHome/main/views.py
+++ turkisHome/main/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'main/index.html')
+
+def flats(request):
+    return render(request, 'main/flats.html')
+
+def new(request):
+    return render(request, 'main/new.html')
