d3635f5b3cfd8e58fc438109c22f7a79d7bfbd1c
janetuyy <118744357+janetuyy@users.noreply.github.com>
2023-08-15 03:28:22 +0300

    eleventh meow

--- turkisHome/main/static/main/js/script.js
+++ turkisHome/main/static/main/js/script.js
@@ -71,3 +71,22 @@ $(document).ready(function() {
         nextArrow: '<button type = "button" class = "slick-next"></ button>'
     });
 })
+
+function menuFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
